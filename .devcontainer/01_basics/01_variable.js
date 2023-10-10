4f113bc643ea6e7e607aceaf17d2aaa9bc6db2c1
Prasoon Pandey <138553057+Prasoon31Pandey@users.noreply.github.com>
2023-10-10 15:01:52 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_variable.js
@@ -0,0 +1,19 @@
+const accountId = 14453
+let accountEmail = "prasoon@google.com"
+var accountpassw = "233"
+accountcity = "Jaipur"
+let accountstate;
+
+//account Id=2
+
+accountEmail = "hcfca@gmail.com"
+accountpassw = "123345"
+accountcity = "london"
+
+console.log(accountId);
+
+console.table([accountEmail, accountpassw, accountcity, accountstate])
+
+/*
+prefer not to use war
+*/
---
+++ .devcontainer/01_basics/02-databases.js
@@ -0,0 +1,27 @@
+"use strict" ; //treat all js code as new version
+
+// alert(9+9); // we are using js, not browser
+console.log(2+2); // code readeilibity should be high
+
+console.log("Prasoon")
+
+let name ="Prasoon"
+let age = 21
+let isLoggedIN = true
+let state;
+
+// number => 2 to power 53
+//bigint
+//string => ""
+
+//boolean => true/false
+
+//null => standalone value
+// undefined =>
+// symbol = unique;
+
+//object
+
+console.log(typeof"Prasoon");
+console.log(typeof"null");//object
+console.log(typeof"undefined");//indefined
